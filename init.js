0751814dbab4d8c2772f1cd5304dc5f0bd574994
Julien Bidoret <julienbidoret@gmail.com>
2019-12-16 23:49:56 +0100

    NON

--- init.js
+++ init.js
@@ -1,14 +1,14 @@
 const artengreve = "Art en grève";
 const colere = artengreve.split("");
-const cri = document.querySelector('h1');
+const cri = document.querySelector('#content');
 
 colere.forEach(function(voix, idx){
-    let echo = voix == " " ? 2 : 5;
+    let echo = voix == " " ? 1 : 5;
     const slogan = Math.floor(Math.random() * echo);
     for (var i = 0; i <= echo; i++) {
         let rage = document.createElement('span');
         rage.textContent = voix;
-        if(i != slogan) rage.style.height = Math.ceil(Math.random() * 1.8 + .3) + "cm";
+        if(i != slogan) rage.style.height = Math.ceil(Math.random() * 1.6 + .3) + "cm";
         cri.appendChild(rage);
     }
 })
@@ -16,3 +16,12 @@ colere.forEach(function(voix, idx){
 const generale_et_inconditionnelle = document.createElement('span');
 generale_et_inconditionnelle.textContent = "!!!"
 cri.appendChild(generale_et_inconditionnelle);
+
+// Bindery.js
+Bindery.makeBook({
+    content: '#content',
+    pageSetup: {
+      size: { width: '80mm', height: '1220mm' },
+      margin: { top: '12pt', inner: '12pt', outer: '16pt', bottom: '20pt' },
+  }
+});
